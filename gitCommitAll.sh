52e399916e2c69eee40825b862d7948a9f443b2c
Sharon <ali93456@gmail.com>
2023-05-23 04:32:39 +0300

    Testing my script

---
+++ gitCommitAll.sh
@@ -0,0 +1,43 @@
+#!/bin/bash
+
+add_and_commit()
+{
+    message="$1"
+    echo "Adding and Committing ALL changes..."
+    echo "Your Committing message is: $message"
+    git add . && git commit -m "$message"
+}
+
+push() 
+{
+    echo "pushing"
+    # git push -u origin main
+}
+
+
+while getopts ha:p flag; 
+do
+  case $flag in
+    h)
+      echo "help me pls"
+      ;;
+
+    a)
+      message=$OPTARG
+      add_and_commit "$message"
+      ;;
+
+    p)
+      push
+      exit 0
+      ;;
+
+    \?)
+      echo "NON existent flag, sorry"
+      exit 1
+      ;;
+
+  esac # END of case
+done
+
+
