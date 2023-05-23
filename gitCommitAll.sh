1ba5300196dc2e97b26d04455b99c73bc734cbec
Sharon <ali93456@gmail.com>
2023-05-23 17:05:18 +0300

    formating the file

--- gitCommitAll.sh
+++ gitCommitAll.sh
@@ -1,57 +1,53 @@
 #!/bin/bash
 
-add_and_commit()
-{
-    message="$1"
-    echo ""
-    echo "Adding and Committing ALL changes..."
-    echo "Your Committing message is: $message"
-    echo ""
-    git add . && git commit -m "$message"
-    echo ""
-    echo ""
+add_and_commit() {
+  message="$1"
+  echo ""
+  echo "Adding and Committing ALL changes..."
+  echo "Your Committing message is: $message"
+  echo ""
+  git add . && git commit -m "$message"
+  echo ""
+  echo ""
 }
 
-push() 
-{
-    echo "Pushing changes..."
-    git push -u origin main
+push() {
+  echo "Pushing changes..."
+  git push -u origin main
 }
 
 help() {
-      echo "The Usage of this script is: "
-      echo -e "    -h To get help."
-      echo -e "    -a To add and commit all files in current repo (You need to pass a commit message)."
-      echo -e "    For Example: \"./gitCommitAll.sh -a 'this is my commit message'\".\n"
-      echo -e "    -p If you want to push the changes you committed add this flag in the end."
-      echo -e "    For Example: \"./gitCommitAll.sh -a 'this is my commit message' -p\".\n"
-      echo ""
+  echo "The Usage of this script is: "
+  echo -e "    -h To get help."
+  echo -e "    -a To add and commit all files in current repo (You need to pass a commit message)."
+  echo -e "    For Example: \"./gitCommitAll.sh -a 'this is my commit message'\".\n"
+  echo -e "    -p If you want to push the changes you committed add this flag in the end."
+  echo -e "    For Example: \"./gitCommitAll.sh -a 'this is my commit message' -p\".\n"
+  echo ""
 }
 
-while getopts ha:p flag; 
-do
+while getopts ha:p flag; do
   case $flag in
-    h)
-        help
-        exit 0
-      ;;
+  h)
+    help
+    exit 0
+    ;;
 
-    a)
-      message=$OPTARG
-      add_and_commit "$message"
-      ;;
+  a)
+    message=$OPTARG
+    add_and_commit "$message"
+    ;;
 
-    p)
-      push
-      exit 0
-      ;;
+  p)
+    push
+    exit 0
+    ;;
 
-    \?)
-      echo "NON existent flag, sorry"
-      help 
-      exit 1
-      ;;
+  \?)
+    echo "NON existent flag, sorry"
+    help
+    exit 1
+    ;;
 
   esac # END of case
 done
-
